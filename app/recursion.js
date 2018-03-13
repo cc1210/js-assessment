ebd65e670a2dd19d70a418d22496d240235b0a1b
unknown <chapfan42@gmail.com>
2018-03-12 22:04:06 -0700

    Added answers to the module unit tests, recursion unit tests, and regex unit tests

--- app/recursion.js
+++ app/recursion.js
@@ -2,15 +2,57 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+    var fileList = [];
+    function getAllFiles(data) {
+      var values = data.files;
+      for (var i = 0; i < values.length; i++) {
+        if (typeof values[i] === 'object') {
+          getAllFiles(values[i]);
+        } else if (typeof values[i] === 'string') {
+          fileList.push(values[i]);
+        }
+      }
+    }
+    function getSubDirFiles(theData, directory) {
+      if (theData.dir === directory) {
+        getAllFiles(theData);
+      } else {
+        for (var j = 0; j < theData.files.length; j++) {
+          if (typeof theData.files[j] === 'object') {
+            getSubDirFiles(theData.files[j], directory);
+          }
+        }
+      }
+    }
+    if (dirName === undefined) {
+      getAllFiles(data);
+    } else {
+      getSubDirFiles(data, dirName);
+    }
+    return fileList;
   },
 
   permute: function(arr) {
-
+    var resultArray = [];
+    var usedElements = [];
+    function startThePerm(elem) {
+      
+    }
   },
 
   fibonacci: function(n) {
-
+    var fibArray = [0, 1];
+    fibArray.reverse();
+    function buildFibSeq(current, previous) {
+      fibArray.unshift(current + previous);
+      var len = fibArray.length;
+      if (fibArray.length !== (n + 1)) {
+        buildFibSeq(fibArray[0], fibArray[1]);
+      }
+    }
+    buildFibSeq(fibArray[1], fibArray[0]);
+    fibArray.reverse();
+    return fibArray[n];
   },
 
   validParentheses: function(n) {
--- app/regex.js
+++ app/regex.js
@@ -2,23 +2,47 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    var re = /\d/;
+    return re.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    var status;
+    for (var i = 0; i < str.length; i++) {
+      var re = new RegExp(str[i] + '{2,}');
+      var re2 = new RegExp(/\D/);
+      if (re.test(str) && re2.test(str[i])) {
+        status = true;
+        break;
+      } else {
+        status = false;
+      }
+    }
+    return status;
   },
 
   endsWithVowel: function(str) {
-
+    var re = new RegExp(/[aeiouAEIOU]$/);
+    return re.test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    var re = new RegExp(/(\d\d\d)/);
+    if (str.match(re)) {
+      return str.match(re)[1];
+    } else {
+      return false;
+    }
   },
 
   matchesPattern: function(str) {
-
+    var re = new RegExp(/(\d\d\d)(?=-)(\d\d\d)(?=-)/);
+    var status = str.match(re);
+    if (str.match(re)) {
+      return true;
+    } else {
+      return false;
+    }
   },
 
   isUSD: function(str) {
