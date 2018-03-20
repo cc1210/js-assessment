3793d03c2dccd181b2fb7e0847f19a4117ac37ab
unknown <chapfan42@gmail.com>
2018-03-19 21:49:13 -0700

    Adding the last test for regular expressions module completion

--- app/regex.js
+++ app/regex.js
@@ -41,9 +41,7 @@ exports.regexAnswers = {
   },
 
   isUSD: function(str) {
-    // var re = new RegExp(/^\$(\d{1,3},{0,1})\.\d{2}$/);
-    var re = new RegExp(/,/);
-    var status = str.match(re);
-    return status;
+    var re = new RegExp(/^\$\d{1,3}(,\d{3})*(\.\d{2})?$/);
+    return re.test(str);
   }
 };
