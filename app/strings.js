c743540b489c0698a34739b16fc439eff821d2a5
unknown <chapfan42@gmail.com>
2018-05-20 20:58:58 -0700

    Adding solutions to the reducedString and reverseString tests

--- app/strings.js
+++ app/strings.js
@@ -2,20 +2,56 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    var strArray = str.split('');
     var obj = {};
-    var strArray = [];
-    var newString = "";
-    for (var i = 0; i < str.length; i++) {
-      var letter = str[i];
-
-      if (count < amount && ) {
-
+    var reducedString = "";
+    var conseqArray = [];
+    var counter = 0;
+    function findConsecutiveLetters (sequence) {
+      var strIterator = sequence.values();
+      var currentLetter = strIterator.next().value;
+      while (sequence.length > 1) {
+        var nextLetter = strIterator.next().value;
+        if ( currentLetter[0] === nextLetter ) {
+          if (currentLetter.length === 1) {
+            currentLetter += counter.toString();
+          }
+          sequence[1] = nextLetter + counter.toString();
+        } else {
+          counter++;
+        }
+        conseqArray.push(currentLetter);
+        sequence.splice(0, 1);
+        findConsecutiveLetters(sequence);
+      }
+      conseqArray.push(currentLetter);
+    }
+    findConsecutiveLetters(strArray);
+    function addNTimes(amount, letter) {
+      i = 0;
+      while (i < amount) {
+        reducedString += letter[0];
+        i++;
       }
     }
-    for (var i = 0; i < strArray.length; i++) {
+    !function(array) {
+      array.forEach(function(element, index) {
+        if (obj[element] === undefined) {
+          obj[element] = [index];
+        } else {
+          obj[element].push(index);
+        }
+      });
+    }(conseqArray);
+    Object.keys(obj).forEach(function(key) {
+      if (obj[key].length > amount) {
+        addNTimes(amount, key);
+      } else {
+        reducedString += key;
+      }
 
-    }
-    return str.length;
+    });
+    return reducedString;
   },
 
   wordWrap: function(str, cols) {
@@ -23,6 +59,7 @@ exports.stringsAnswers = {
   },
 
   reverseString: function(str) {
-
+    strArray = str.split('');
+    return strArray.reverse().join('');
   }
 };
